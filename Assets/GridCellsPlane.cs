7128b697848413813722a7992209b9691d61f8b1
Diana <edvi.diana@gmail.com>
2026-05-21 15:33:42 -0400

    feat: created grid with cells to be able to visualize where the rooms are being placed in the future

---
+++ Assets/GridCellsPlane.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GridCellsPlane : MonoBehaviour
+{
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    private float cellSize;
+    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] private Material cellEvenMaterial;
+    [SerializeField] private Material cellOddMaterial;
+
+    void Awake()
+    {
+        cellSize = cellPrefab.GetComponent<Renderer>().bounds.size.x;
+        Debug.Log($"Cell size: {cellSize}");
+        GenerateGrid();
+    }
+    void GenerateGrid()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject cell = GameObject.Instantiate(cellPrefab, new Vector3(i * cellSize, 0, j * cellSize), Quaternion.identity, this.transform);
+                cell.name = $"Cell_{i}_{j}";
+                Renderer cellRenderer = cell.GetComponent<Renderer>();
+                if ((i + j) % 2 == 0)
+                {
+                    cellRenderer.material = cellEvenMaterial;
+                }
+                else
+                {
+                    cellRenderer.material = cellOddMaterial;
+                }
+            }
+        }
+    }
+}
