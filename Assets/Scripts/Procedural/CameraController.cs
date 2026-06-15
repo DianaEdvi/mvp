59420cdde6da48eacd19e513108eee6d33c075c1
Diana <115322193+DianaEdvi@users.noreply.github.com>
2026-06-15 00:07:16 -0400

    feat: Implemented corner camera logic (#17)
    * feat: added in camera transforms to rooms
    
    * update: moved the camera to the latest room and did a change of basis function to get the player's movement to be relative to the room camera
    
    * feat: implemented camera room logic
    
    * update: added colliders and cameras to all the rooms

---
+++ Assets/Scripts/Procedural/CameraController.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraController : MonoBehaviour
+{
+    [Tooltip("The Cinemachine camera specific to this room.")]
+    [SerializeField] private CinemachineCamera roomCamera;
+    
+    [Tooltip("Priority when the player is inside the room.")]
+    [SerializeField] private int activePriority = 20;
+    
+    private int defaultPriority;
+    private Collider roomCollider;
+
+    private void Start()
+    {
+        if (roomCamera != null)
+        {
+            // Store whatever baseline priority you set in the inspector
+            defaultPriority = roomCamera.Priority.Value;
+        }
+
+        roomCollider = GetComponent<Collider>();
+        
+        // Set the first camera position
+        SnapCameraIfPlayerIsInside();
+    }
+
+    private void SnapCameraIfPlayerIsInside()
+    {        
+        if (roomCollider == null || roomCamera == null) return;
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            // Check if the player is withing the bounds of the room and set the camera to it if yes 
+            if (roomCollider.bounds.Contains(player.transform.position))
+            {
+                roomCamera.Priority.Value = activePriority;
+                if (Camera.main != null)
+                {
+                    Camera.main.transform.SetPositionAndRotation(roomCamera.transform.position, roomCamera.transform.rotation);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && roomCamera != null)
+        {
+            // Set new camera priority 
+            roomCamera.Priority.Value = activePriority;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && roomCamera != null)
+        {
+            // Revert to default priority
+            roomCamera.Priority.Value = defaultPriority;
+        }
+    }
+}
