b677100e8f7fe183c14b07b9732ecebcc3011210
Diana <edvi.diana@gmail.com>
2026-07-18 18:59:10 -0400

    update: implemented basic ui popup when within range

--- Assets/Scripts/NPC/NPC_Asset.cs
+++ Assets/Scripts/NPC/NPC_Asset.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC_Asset : MonoBehaviour
 {
     public NPC_Data npcData;
+    [SerializeField] private Image image;
+
+    [Header("Interaction UI")]
+    [SerializeField] private float displayRange = 5f;
 
     private string npcName;
 
@@ -12,11 +17,41 @@ public class NPC_Asset : MonoBehaviour
         {
             npcName = npcData.npcName;
         }
+
+        if (image != null)
+        {
+            image.gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Player.Instance != null && image != null)
+        {
+            // Calculate the distance between the NPC and the Player
+            float distance = Vector3.Distance(transform.position, Player.Instance.transform.position);
+
+            bool isWithinRange = distance <= displayRange;
+
+            if (image.gameObject.activeSelf != isWithinRange)
+            {
+                image.gameObject.SetActive(isWithinRange);
+            }
+        }
     }
 
     public void Interact()
     {
         Debug.Log($"Interacting with NPC: {npcName}");
         // Add interaction logic here, e.g., dialogue
+    }
+
+    void LateUpdate()
+    {
+        // Only force the rotation math if the image is actually visible
+        if (image != null && image.gameObject.activeSelf)
+        {
+            image.gameObject.transform.rotation = Camera.main.transform.rotation;
+        }
     }
 }
