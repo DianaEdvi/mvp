7a8550f3604db66d0bdd9a6c0d0306081c185381
Diana <edvi.diana@gmail.com>
2026-07-19 01:08:51 -0400

    feat: created combat interaction on e

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Interaction : MonoBehaviour
 {
@@ -68,6 +69,10 @@ public class Interaction : MonoBehaviour
                 {
                     npc.Interact();
                 }
+                break;
+            case "Combat":
+                Debug.Log("Load combat scene");
+                SceneManager.LoadScene("CombatPrototype");
                 break;
 
             default:
